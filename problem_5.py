90cf6595fcb1bfc58969748098024334e98c6e4b
Yurii <maydan90@gmail.com>
2020-09-01 16:34:47 -0500

    Added problem 4 (not yet done).

---
+++ problem_5.py
@@ -0,0 +1,25 @@
+# Represents a single node in the Trie
+class TrieNode:
+    # Initialize this node in the Trie
+    def __init__(self):
+        pass
+
+    # Add a child node in this Trie
+    def insert(self, char):
+        pass
+
+
+# The Trie itself containing the root node and insert/find functions
+class Trie:
+    # Initialize this Trie (add a root node)
+    def __init__(self):
+        pass
+
+    # Add a word to the Trie
+    def insert(self, word):
+        pass
+
+    # Find the Trie node that represents this prefix
+    def find(self, prefix):
+        pass
+
