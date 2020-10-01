9b8b499c6b0af3c94dac6727ed7439978cbf3eae
Yurii <maydan90@gmail.com>
2020-10-01 12:17:27 -0500

    Problem 4 solution updated.

--- problem_4.py
+++ problem_4.py
@@ -8,11 +8,22 @@ def sort_012(input_list):
     # trivial cases of empty array or one element array
     if not input_list or len(input_list) == 1:
         return input_list
+
     last_zero_idx = 0
     first_two_idx = len(input_list) - 1
     current_idx = 0
-    while True:
-        pass
+
+    while current_idx <= first_two_idx:
+        if input_list[current_idx] == 0:
+            input_list[current_idx], input_list[last_zero_idx] = input_list[last_zero_idx], 0
+            last_zero_idx += 1
+            current_idx += 1
+        elif input_list[current_idx] == 2:
+            input_list[current_idx], input_list[first_two_idx] = input_list[first_two_idx], 2
+            first_two_idx -= 1
+        else:
+            current_idx += 1
+    return input_list
 
 
 def test_function(test_case):
@@ -24,6 +35,11 @@ def test_function(test_case):
         print("Fail")
 
 
+test_function([])
+test_function([0])
+test_function([1])
+test_function([2])
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([2 for _ in range(1000)] + [1 for _ in range(1000)] + [0 for _ in range(1000)])
